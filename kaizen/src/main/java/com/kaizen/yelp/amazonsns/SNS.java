87a7ee8776141611f29fee446301588746f831de
Swetha <swetha.patnala@gmail.com>
2014-04-15 00:11:02 -0700

    added publish method

--- kaizen/src/main/java/com/kaizen/yelp/amazonsns/SNS.java
+++ kaizen/src/main/java/com/kaizen/yelp/amazonsns/SNS.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.CreateTopicRequest;
 import com.amazonaws.services.sns.model.CreateTopicResult;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.amazonaws.services.sns.model.SubscribeRequest;
 import com.amazonaws.services.sns.model.SubscribeResult;
 
@@ -13,12 +15,19 @@ public class SNS {
 
 	public static void main(String[] args){
 
-		final AmazonSNS sns = new AmazonSNSClient(new BasicAWSCredentials("<accesskey>",  "<secret key>"));
-
-		sns.setEndpoint("sns.us-west-1.amazonaws.com");
+		final AmazonSNS snsConnection = new AmazonSNSClient(new BasicAWSCredentials("<accesskey>",  "<secret key>"));
+		
+		
+		snsConnection.setEndpoint("sns.us-west-1.amazonaws.com");
+		
+		SNS sns = new SNS();
+		//String topicArn = sns.createTopic(snsConnection);
+		//sns.subscribeToTopic(snsConnection, topicArn, "email", "swetha.patnala@gmail.com");
+		sns.publishToTopic(snsConnection, "arn:aws:sns:us-west-1:416012710802:SwethaTopic1", "testing", "test msg");
+		
 
 	}
-	public String createTopic(AmazonSNS sns){
+	public static String createTopic(AmazonSNS sns){
 
 		CreateTopicRequest createTopicRequest = new CreateTopicRequest("SwethaTopic1");
 		CreateTopicResult created = sns.createTopic(createTopicRequest);
@@ -31,12 +40,18 @@ public class SNS {
 
 	public void subscribeToTopic(AmazonSNS sns , String topicArn , String protocol , String endpoint){
 		
-		//sns.subscribe(topicArn, protocol, endpoint);
+		sns.subscribe(topicArn, protocol, endpoint);
 		//sns.subscribe(topicArn, "email", "swetha.patnala@gmail.com");
 	}
 
 	
-	public void publishToTopic(){
+	public void publishToTopic(AmazonSNS sns ,String topicArn , String message , String subject){
+		
+		PublishRequest publishToTopic = new PublishRequest(topicArn, message , subject);
+		PublishResult publishResult = sns.publish(publishToTopic);
+		publishResult.getMessageId();
+		
+		System.out.println("message id "+ publishResult.getMessageId());
 		
 	}
 
