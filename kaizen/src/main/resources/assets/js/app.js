14db787652754f2723d6810d7a1ba90db471b05b
dhanyabalasundaran <dhanyabalasundaran@gmail.com>
2014-04-14 22:45:20 -0700

    app.js and login

--- kaizen/src/main/resources/assets/js/app.js
+++ kaizen/src/main/resources/assets/js/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-	alert("Hello")
-	$("#login").click(function() {
-		var username = $('#username').val();
+       $("#login").click(function() {
+               var username = $('#username').val();
 
-	var password = $('#password').val();
-	alert(username);
-	$.ajax({		
-	    	type:'GET',
-	    	url:'http://localhost:8080/kaizen/validate',
-	    	contentType: 'application/json',
-			dataType: 'json',
-			data:{
-				"username":username,
-				"password":password
-				
-			},
-			success:function(response){
-				alert(response);
-			}
-	    	
-	    	
-	    });
+       var password = $('#password').val();
+       alert(username);
+
+//	$.ajax({		
+//	    	type:'GET',
+//	    	url:'http://localhost:8080/kaizen/validate',
+//	    	contentType: 'application/json',
+//			dataType: 'json',
+//			data:{
+//				"username":username,
+//				"password":password
+//				
+//			},
+//			success:function(response){
+//				alert(response);
+//			}
+//	    	
+//	    	
+//	    });
 	});	
 	});
