65b8d9d9429b2c1e905bd72c1a70a96ad161aabc
nimmicv <nimmicv@gmail.com>
2014-04-16 09:18:45 -0700

    commit

--- kaizen/src/main/resources/assets/js/home.js
+++ kaizen/src/main/resources/assets/js/home.js
@@ -1,29 +1,5 @@
 
-	$("#search").click(function() {
-		var strData={"name":search};
-alert(search);
-		$.ajax({
-			type : 'POST',
-			url : '/kaizen/business',
-			contentType : 'application/json',
-			dataType : 'json',
-			data:JSON.stringify(strData),
-			success : function(response) {
-				 alert("Response: " + response);
-				 parent.document.getElementById("mustFrame").src="http://localhost:8080/business/"+username;
-				    //$("#mustFrame",parent.document).load("http://localhost:8080/home/"+username);
-			
-				//alert("you are logged in!");
-				//location.href = "http://localhost:8080/home/"+username;
-			},
-			error:function(response)
-			{
-				$.get("http://localhost:8080/home/"+username,function(data,status){
-				    alert("Data: " + data + "\nStatus: " + status);
-				    $(body).html(response);
-				  });
-				
-				}
-		});
+	$("#find").click(function() {
+		alert("hi inside find");
 	});
 
