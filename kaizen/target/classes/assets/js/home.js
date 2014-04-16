432c0f0a62c4f6dacf803bad404419731d2b6772
nimmicv <nimmicv@gmail.com>
2014-04-16 09:47:59 -0700

    Commits

--- kaizen/target/classes/assets/js/home.js
+++ kaizen/target/classes/assets/js/home.js
@@ -1,20 +1,17 @@
 
-<<<<<<< HEAD
-	$("#find").click(function() {
-		alert("hi inside find");
-=======
 	$("#search").click(function() {
+		$( "#businessTable" ).css('display', 'block');
 		var name = $('#name').val();
 		
 		$.ajax({
 			type : 'GET',
-			url : '/kaizen?name=name',
+			url : '/kaizen/business',
 			contentType : 'application/json',
 			dataType : 'json',
 			data:{"name":name},
 			success : function(response) {
 				 alert("Response: " + response);
-				 parent.document.getElementById("mustFrame").src="http://localhost:8080/business/"+name;
+				 //parent.document.getElementById("mustFrame").src="http://localhost:8080/business?name="+name;
 				    //$("#mustFrame",parent.document).load("http://localhost:8080/home/"+username);
 			
 				//alert("you are logged in!");
@@ -24,6 +21,5 @@
 			{
 			}
 		});
->>>>>>> FETCH_HEAD
 	});
 
