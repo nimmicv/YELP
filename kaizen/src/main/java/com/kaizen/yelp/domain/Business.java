a7908b0ff31c2256aed89d8699802ab86393064b
jain-parul <jainparul.2189@gmail.com>
2014-05-03 15:20:50 -0700

    Complete Search with user preferences and field validations (added with your changes)

--- kaizen/src/main/java/com/kaizen/yelp/domain/Business.java
+++ kaizen/src/main/java/com/kaizen/yelp/domain/Business.java
@@ -9,11 +9,52 @@ private String name;
 private String full_address;
 private String hours;
 private String categories;
-private float review_count;
 private float stars;
 private String longitude;
 private String latitude;
+private String parking;
+private String creditcards;
+private String takeout;
+private String wifi;
+private String kids;
+private String groups;
 
+public String getParking() {
+	return parking;
+}
+public void setParking(String parking) {
+	this.parking = parking;
+}
+public String getCreditcards() {
+	return creditcards;
+}
+public void setCreditcards(String creditcards) {
+	this.creditcards = creditcards;
+}
+public String getTakeout() {
+	return takeout;
+}
+public void setTakeout(String takeout) {
+	this.takeout = takeout;
+}
+public String getWifi() {
+	return wifi;
+}
+public void setWifi(String wifi) {
+	this.wifi = wifi;
+}
+public String getKids() {
+	return kids;
+}
+public void setKids(String kids) {
+	this.kids = kids;
+}
+public String getGroups() {
+	return groups;
+}
+public void setGroups(String groups) {
+	this.groups = groups;
+}
 public String getName() {
         return name;
 }
@@ -45,12 +86,6 @@ public String getHours() {
 }
 public void setHours(String hours) {
 	this.hours = hours;
-}
-public float getReview_count() {
-	return review_count;
-}
-public void setReview_count(float review_count) {
-	this.review_count = review_count;
 }
 public float getStars() {
 	return stars;
