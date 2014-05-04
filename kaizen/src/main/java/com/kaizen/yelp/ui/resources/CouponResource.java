eba5cb8ac45605bde297f74a1213f879738acb51
Swetha <swetha.patnala@gmail.com>
2014-05-04 15:56:55 -0700

    added coupon search based on zipcode

--- kaizen/src/main/java/com/kaizen/yelp/ui/resources/CouponResource.java
+++ kaizen/src/main/java/com/kaizen/yelp/ui/resources/CouponResource.java
@@ -23,6 +23,7 @@ import org.json.JSONArray;
 import org.json.JSONTokener;
 
 import com.amazonaws.http.HttpResponse;
+import com.kaizen.yelp.coupon.Coupon;
 
 //import com.mashape.unirest.http.HttpResponse;
 //import com.mashape.unirest.http.JsonNode;
@@ -69,26 +70,26 @@ public class CouponResource {
 
 
 }
-*/
+	 */
 
-	
-	
+
+
 	@GET
 	@Path("/test")
 
 	public Response getTest() throws  Exception{
 		System.out.println(" hello world");
-		
+
 		HttpClient client1 = new DefaultHttpClient();
-		
-		
+
+
 		HttpGet request1 = new HttpGet("http://api.8coupons.com/v1/getsubcategory");
 		org.apache.http.HttpResponse response = client1.execute(request1);
-		
-		
-		
+
+
+
 		System.out.println(" response" + response.getEntity().getContent().toString());
-		
+
 		BufferedReader reader = new BufferedReader  (new InputStreamReader(( response).getEntity().getContent()));
 		StringBuilder allContents = new StringBuilder();
 
@@ -98,46 +99,96 @@ public class CouponResource {
 			allContents.append(line);
 		}
 		System.out.println(" string value :"+ allContents.toString());
-		
-	
+
+
 		//allContents.toString().replaceAll("\\s", "");
-//		
-//		System.out.println(" new output value:" + allContents.toString().replaceAll("\\s", ""));
-//		//allContents.replace(arg0, arg1, arg2)
-//		
-//		String input = allContents.toString().replaceAll("\\s", "");
-//		InputStream is = new ByteArrayInputStream(input.getBytes());
-//		 
-//		// read it with BufferedReader
-//		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-	 
-//		JSONTokener tokener = new JSONTokener(br);
-//		JSONArray finalResult = new JSONArray(tokener);
-//		
-		
+		//		
+		//		System.out.println(" new output value:" + allContents.toString().replaceAll("\\s", ""));
+		//		//allContents.replace(arg0, arg1, arg2)
+		//		
+		//		String input = allContents.toString().replaceAll("\\s", "");
+		//		InputStream is = new ByteArrayInputStream(input.getBytes());
+		//		 
+		//		// read it with BufferedReader
+		//		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		//		JSONTokener tokener = new JSONTokener(br);
+		//		JSONArray finalResult = new JSONArray(tokener);
+		//		
+
 		String userSelectedCategory = "Moroccan";
 		
-		String categoryId;
 		
+
+		String categoryIdFromCouponAPI = "";
+
 		JSONArray arr = new JSONArray(allContents.toString());
 		for (int i =0 ; i < arr.length() ; i++){
 			//System.out.println(" array deatils :" + arr.getJSONObject(i).get("category"));
-			
+
 			String sub = (String) arr.getJSONObject(i).get("subcategory");
 			//System.out.println(" sub category" + sub);
-			
+
 			if ( sub.equals(userSelectedCategory)){
-				categoryId = (String) arr.getJSONObject(i).get("subcategoryID");
+				categoryIdFromCouponAPI = (String) arr.getJSONObject(i).get("subcategoryID");
 				System.out.println(" id is " + arr.getJSONObject(i).get("subcategoryID"));
-				
+
 			}
 		}
-		
-		
+
+
+		String baseUrl = "http://api.8coupons.com/v1/getdeals?key=8871a60a47c12653695c681668f16e7a525d6bffc8c4af98c18a92e6e35578696c62b8cea9bf4f32c444ce507b243c39";
+
+		String include = "&mileradius=20&limit=5&orderby=radius";
+
+		String zip = "85233";
+		String zipQuery = "&zip="+zip;
+		String categoryQuery = "&subcategoryid="+categoryIdFromCouponAPI;
+
+		String fullQuery = baseUrl + zipQuery + include + categoryQuery;
+
+		System.out.println(" full query" + fullQuery);
+
+
+
+		HttpClient client2 = new DefaultHttpClient();
+
+
+		HttpGet request2 = new HttpGet(fullQuery);
+		org.apache.http.HttpResponse response2 = client1.execute(request2);
+
+
+
+		System.out.println(" response" + response2.getEntity().getContent().toString());
+
+		BufferedReader reader2 = new BufferedReader  (new InputStreamReader(( response2).getEntity().getContent()));
+		StringBuilder allContents2 = new StringBuilder();
+
+		String line2 = null;
+
+		while ((line2 = reader2.readLine()) != null) {
+			allContents2.append(line2);
+		}
+
+		JSONArray arr2 = new JSONArray(allContents2.toString());
+
+
+		for (int i =0 ; i < arr2.length(); i++){
+
+			System.out.println(" deal title :" +  arr2.getJSONObject(i).get("dealTitle"));
+			System.out.println(" deal source :" +arr2.getJSONObject(i).get("dealSource"));
+
+		}
 
 		return Response.status(200).build();
 
+
+
 		
 		
+		
+
+
+
 	}
 }
