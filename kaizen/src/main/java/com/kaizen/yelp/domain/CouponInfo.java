97ba3270b6ae48c5025eb2bc7ed6a693fb172a90
dhanyabalasundaran <dhanyabalasundaran@gmail.com>
2014-05-06 19:33:10 -0700

    coupons updated

--- kaizen/src/main/java/com/kaizen/yelp/domain/CouponInfo.java
+++ kaizen/src/main/java/com/kaizen/yelp/domain/CouponInfo.java
@@ -1,24 +1,66 @@
 package com.kaizen.yelp.domain;
 
 public class CouponInfo {
-	
+
 	private String dealName;
 	private String dealSource;
 	private String dealImage;
 	private String dealLink;
-	
-	
+
+
 	public CouponInfo(String dealName , String dealSource , String dealImage, String dealLink) {
 		this.dealName = dealName;
 		this.dealSource = dealSource;
 		this.dealImage = dealImage;
 		this.dealLink = dealLink;
-		
+
 	}
-	@Override
-	public String toString(){
-		
-		return dealName + "***" + dealSource + "***" + dealImage + "***" + dealLink;
+//	@Override
+//	public String toString(){
+//
+//		return dealName + "*" + dealSource + "*" + dealImage + "*" + dealLink;
+//
+//	}
+
+
+	public String getDealName() {
+		return dealName;
 	}
 
+
+	public void setDealName(String dealName) {
+		this.dealName = dealName;
+	}
+
+
+	public String getDealSource() {
+		return dealSource;
+	}
+
+
+	public void setDealSource(String dealSource) {
+		this.dealSource = dealSource;
+	}
+
+
+	public String getDealImage() {
+		return dealImage;
+	}
+
+
+	public void setDealImage(String dealImage) {
+		this.dealImage = dealImage;
+	}
+
+
+	public String getDealLink() {
+		return dealLink;
+	}
+
+
+	public void setDealLink(String dealLink) {
+		this.dealLink = dealLink;
+	}
+
+	
 }
